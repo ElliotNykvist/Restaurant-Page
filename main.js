6680301196a5ad2e9ac549a50d47961cd782ea8f
Elliot <elliotnykvist@hotmail.se>
2023-08-10 18:07:46 +0200

    Added scroll effect to the navbar

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+window.addEventListener("scroll", function() {
+  var scrollTrigger = document.querySelector(".scroll-trigger");
+  var nav = document.querySelector("nav");
+
+  if (window.scrollY > scrollTrigger.offsetTop) {
+    nav.classList.add("scrolled-nav");
+  } else {
+    nav.classList.remove("scrolled-nav");
+  }
+});
