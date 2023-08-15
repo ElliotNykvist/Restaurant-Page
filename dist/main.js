9d34066859b0fd498fe3b0fa7f3fde0a9e5cc1c2
Elliot <elliotnykvist@hotmail.se>
2023-08-15 18:14:20 +0200

    Added hover effect and change font

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function() {
   var scrollTrigger = document.querySelector(".scroll-trigger");
-  var nav = document.querySelector("nav");
+  var nav = document.querySelector(".nav");
 
   if (window.scrollY > scrollTrigger.offsetTop) {
     nav.classList.add("scrolled-nav");
